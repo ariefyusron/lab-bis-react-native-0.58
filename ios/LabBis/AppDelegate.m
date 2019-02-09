fefb20a0444792237b6b61dee2772869431a0c14
Arief Yusron <ariefyusron98@gmail.com>
2019-02-10 05:27:58 +0700

    Rename app

--- ios/LabBis/AppDelegate.m
+++ ios/LabBis/AppDelegate.m
@@ -19,7 +19,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"BoilerPlate"
+                                                      moduleName:@"LabBis"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [UIColor blackColor];
--- ios/LabBisTests/LabBisTests.m
+++ ios/LabBisTests/LabBisTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface BoilerPlateTests : XCTestCase
+@interface LabBisTests : XCTestCase
 
 @end
 
-@implementation BoilerPlateTests
+@implementation LabBisTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
