6a08eda6169f187fb6a8e3a29902f721593003d5
Arief Yusron <ariefyusron98@gmail.com>
2019-02-10 08:34:17 +0700

    crea page class

--- src/class/screens/Class.js
+++ src/class/screens/Class.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
 
 class Class extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: [
+        {name: 'Praktikum Alpro C Praktikum Alpro C',members: '29 members'},
+        {name: 'Praktikum Alpro A',members: '31 members'},
+        {name: 'Praktikum Alpro B',members: '19 members'},
+        {name: 'Praktikum Alpro D',members: '15 members'},
+        {name: 'Praktikum Alpro E',members: '12 members'}
+      ]
+    }
+  }
+
+  _renderListClass = ({item}) => (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.colLeft}>
+        <Text style={styles.nameClass}>{item.name}</Text>
+        <Text style={styles.memberCLass}>{item.members}</Text>
+      </View>
+      <View style={styles.colRight}>
+        <Icon name='chevron-small-right' type='Entypo' style={styles.icon} />
+      </View>
+    </TouchableOpacity>
+  )
   
   render() {
     return (
-      <View>
-        <Text>Class</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={this._renderListClass}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4F4F4',
+    flex: 1,
+    padding: 12
+  },
+  row: {
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    marginBottom: 9,
+    paddingHorizontal: 15,
+    paddingVertical: 9,
+    flexDirection: 'row'
+  },
+  colRight: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  nameClass: {
+    fontSize: 14,
+    color: '#000'
+  },
+  memberCLass: {
+    fontSize: 11,
+    color: '#777777'
+  },
+  icon: {
+    color: '#595959'
+  }
+})
 
 export default Class;
