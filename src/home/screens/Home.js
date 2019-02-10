6cc02f7969a26684b143a286b17564fcb1fef7d9
Arief Yusron <ariefyusron98@gmail.com>
2019-02-10 09:15:05 +0700

    style header Home

--- src/home/screens/Home.js
+++ src/home/screens/Home.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
 
 class Home extends Component {
+  
+  static navigationOptions = {
+    headerTitle: 'Lab BIS',
+    headerTitleStyle: {
+      color: '#0172BB'
+    },
+    headerRight: (
+      <TouchableOpacity>
+        <Icon name='send-o' type='FontAwesome' style={{color: '#0172BB', fontSize: 23, marginRight: 19}} />
+      </TouchableOpacity>
+    )
+  }
   
   render() {
     return (
