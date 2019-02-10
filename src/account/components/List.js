b3fa1b3f7e798b5e6c79f6ee0c13a73de54e51d4
Arief Yusron <ariefyusron98@gmail.com>
2019-02-10 10:09:56 +0700

    style page account

---
+++ src/account/components/List.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
+
+const List = () => (
+  <View>
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.colLeft}>
+        <Text style={styles.title}>Profile</Text>
+      </View>
+      <View style={styles.colRight}>
+        <Icon name='chevron-small-right' type='Entypo' style={styles.icon} />
+      </View>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.colLeft}>
+        <Text style={styles.title}>Security</Text>
+      </View>
+      <View style={styles.colRight}>
+        <Icon name='chevron-small-right' type='Entypo' style={styles.icon} />
+      </View>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.colLeft}>
+        <Text style={styles.title}>Log out</Text>
+      </View>
+      <View style={styles.colRight}>
+        <Icon name='chevron-small-right' type='Entypo' style={styles.icon} />
+      </View>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  row: {
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    marginBottom: 9,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: 'row'
+  },
+  colLeft: {
+  },
+  colRight: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  icon: {
+    color: '#595959'
+  },
+  title: {
+    fontSize: 18,
+    color: '#000'
+  },
+})
+
+export default List;
--- src/account/screens/Account.js
+++ src/account/screens/Account.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+
+import List from '../components/List';
 
 class Account extends Component {
 
@@ -12,11 +14,65 @@ class Account extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Account</Text>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.containerImage}>
+              <Image
+                source={{uri: 'https://lh3.googleusercontent.com/a-/AAuE7mBPLyzzuOx0NFjGW6wD4InDi6aR4KWhpw3sxvgfdg=s640-rw-il'}}
+                style={styles.image}
+              />
+            </TouchableOpacity>
+            <View style={styles.detailProfileContainer}>
+              <Text style={styles.name}>Arief Yusron</Text>
+              <Text style={styles.detail}>160441100023</Text>
+              <Text style={styles.detail}>Fakultas Teknik</Text>
+              <Text style={styles.detail}>Sistem Informasi</Text>
+              <Text style={styles.detail}>2016</Text>
+            </View>
+          </View>
+          <List />
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4F4F4',
+    flex: 1,
+    padding: 12
+  },
+  row: {
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    paddingHorizontal: 33,
+    paddingVertical: 19,
+    flexDirection: 'row',
+    marginBottom: 24
+  },
+  detailProfileContainer: {
+    marginLeft: 23
+  },
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 5
+  },
+  name: {
+    color: '#000',
+    fontSize: 24,
+    fontWeight: '300'
+  },
+  detail: {
+    color: '#777777',
+    fontSize: 14,
+    fontWeight: '200'
+  },
+  containerImage: {
+    justifyContent: 'center'
+  }
+})
 
 export default Account;
